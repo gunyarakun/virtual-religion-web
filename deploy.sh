cf178690669dfcff49bbee11079f3ec522c7a95f
Tasuku SUENAGA a.k.a. gunyarakun <tasuku-s-github@titech.ac>
2021-08-10 00:49:37 +0900

    Fix invalidation bugs

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 aws s3 sync content s3://ja-virtual-religion-web/ --profile wktk_blog
-aws cloudfront create-invalidation --profile wktk_blog --distribution-id E1NWP27GBK6PY2 --paths /ja/
+aws cloudfront create-invalidation --profile wktk_blog --distribution-id E1NWP27GBK6PY2 --paths "/*"
